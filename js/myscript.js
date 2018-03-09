ca6ed764cbc874e866ccce820f669a40086511b5
Ivan Reshetar <ivanreshetar@gmail.com>
2018-03-09 19:28:17 +0200

    change 09-03

---
+++ js/myscript.js
@@ -0,0 +1,11 @@
+var list = ['cat','lion','bear'];
+
+var user = {
+	user: "John",
+	age: 34,
+	hobby: "Soccer",
+	spells: ["hello","bue","nice"],
+	shout: function (){
+		console.log("AHHHH");
+	}
+};
